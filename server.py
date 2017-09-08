3cd926f14b54a532b6a9767b094bb42a7257844f
Chuck Kang <chuckkang@gmail.com>
2017-09-08 11:51:11 -0700

    ninja api

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from flask import jsonify
+
 app = Flask(__name__)
+app.secret_key = "thisisasecret"
 
 @app.route('/')
 def index():
@@ -11,6 +14,22 @@ def tmnt():
 
 @app.route('/ninja/<color>')
 def tmnt_color(color):
+      return render_template("tmnt.html", username)
+
+@app.route('/get_current_user/<color>', methods=['GET'])
+def get_current_user(color):
+    if (color=='red'):
+        name='raphael'
+    elif (color=='blue'):
+        name='donatello'
+    elif (color=='orange'):
+        name='leonardo'
+    elif (color=='purple'):
+        name='michelangelo'
+    else: 
+        name='notapril'
+
+    return jsonify(color=color, name=name)
       
-      return render_template("tmnt.html", color=color)
+      
 app.run(debug=True) # run our server
